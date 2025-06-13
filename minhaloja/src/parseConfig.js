711bfc11c15755095da2c30165b6be387c2e10be
Lucas <vinicioslucas37@hotmail.com>
2025-06-13 08:25:35 -0300

    Preenchendo meu parse com as minhas informações do back

---
+++ minhaloja/src/parseConfig.js
@@ -0,0 +1,6 @@
+import Parse from 'parse';
+
+Parse.initialize("FK7Hl0YbTEBQrWzvMhxLfBUY2EGks06tV0DRnVzV", "QnLLXPatg7SHrnnVvncRhW0BTem3vHfuDisHpMsG");
+Parse.serverURL = "https://parseapi.back4app.com/";
+
+export default Parse;
